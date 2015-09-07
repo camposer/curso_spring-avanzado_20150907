949f9f52580a957fc15983d809c29cf3ae446961
Rodolfo Campos <camposer@gmail.com>
2015-09-07 13:51:37 +0200

    Mejorada la gestión de configuraciones

--- ejercicio1/src/main/java/AppJavaConfig2.java
+++ ejercicio1/src/main/java/AppJavaConfig2.java
@@ -1,21 +1,13 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-import config.BeanConfig;
 import gui.CalculadoraGui;
 
+@ComponentScan(basePackages = { "service", "gui" })
 public class AppJavaConfig2 {
 	public static void main(String[] args) {
-		new AnnotationConfigApplicationContext(Config.class)
+		new AnnotationConfigApplicationContext(AppJavaConfig2.class)
 			.getBean(CalculadoraGui.class)
 			.iniciar();
-	}
-	
-	@Configuration
-	@ComponentScan(basePackages = { "service", "gui" })
-	//@ComponentScan(basePackageClasses = BeanConfig.class)
-	public static class Config {
-		
 	}
 }
