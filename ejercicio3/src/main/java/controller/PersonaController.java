87e045fb811a617105da55e9f4f7372b84932e6f
Rodolfo Campos <camposer@gmail.com>
2015-09-08 16:54:06 +0200

    Agregado enunciado del ejercicio3

--- ejercicio3/src/main/java/controller/PersonaController.java
+++ ejercicio3/src/main/java/controller/PersonaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/persona*")
 public class PersonaController {
-	public static String RUTA_JSP = "/WEB-INF/jsp/persona/index.jsp";
+	public static String RUTA_JSP = "/WEB-INF/jsp/persona/inicio.jsp";
 	
 	@RequestMapping(value = "/inicio", method = RequestMethod.GET)
 	public String listar() {
